e4cf2ecb7fa949b69705510189553e64608d1cb6
LastLeaf <bqfu@163.com>
2016-03-28 23:32:26 +0800

    Initial code.

---
+++ README.md
@@ -0,0 +1,7 @@
+# dbview #
+
+Map database or other data sources to data views, and transform between views.
+
+# Development Status #
+
+Initial prototyping. Do not use it.
---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    List: require('./lib/list'),
+    Item: require('./lib/item'),
+    Watcher: require('./lib/watcher'),
+    Ref: require('./lib/ref'),
+    View: require('./lib/view'),
+};
