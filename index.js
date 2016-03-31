47b8b1db384d5c3f0ca3753dac02a746d3e01589
LastLeaf <bqfu@163.com>
2016-03-31 22:51:40 +0800

    prototyping

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 module.exports = {
-    List: require('./lib/list'),
-    Item: require('./lib/item'),
+    Schema: require('./lib/schema'),
     View: require('./lib/view'),
-    ViewMixin: require('./lib/view_mixin'),
     ViewError: require('./lib/view_error'),
     MemorySource: require('./lib/memory_source'),
 };
