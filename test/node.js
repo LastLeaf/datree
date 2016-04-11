e30c79a1594b87023586fd8be8cf2cd6a8945d50
LastLeaf <bqfu@163.com>
2016-04-11 22:48:36 +0800

    Prototyping.

--- test/node.js
+++ test/node.js
@@ -48,8 +48,13 @@ describe('Node', function(){
             expect(post.getChild('published').getCachedValue()).to.equal(false);
             expect(post.getStaticChild('time').get()).to.equal(0);
             expect(post.content).to.be.equal('CONTENT1');
-            //expect(post.comments.getDynamicChild(0)).to.be.instanceof(Node);
-            // TODO let update(Fields) known by source
+            expect(post.comments.getDynamicChildren().length).to.equal(2);
+            expect(post.comments[0].user._id).to.equal('UID2');
+            expect(post.comments[0].user.name).to.equal('USER2');
+            expect(post.comments[0].content).to.equal('CONTENT2');
+            expect(post.comments[1].user._id).to.equal('UID3');
+            expect(post.comments[1].user.name).to.equal('USER3');
+            expect(post.comments[1].content).to.equal('CONTENT3');
         });
 
     });
